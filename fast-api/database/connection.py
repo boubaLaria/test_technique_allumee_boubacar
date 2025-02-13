6130b8127506507a63c87cec9e95c619fd1f73b9
boubalaria <ladialloboubacar@gmail.com>
2025-02-13 11:49:20 +0100

    connection a une base de donnee postgres sql

---
+++ fast-api/database/connection.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+
+# Création du moteur SQLAlchemy
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+# Création de la session asynchrone
+SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+# Définition de la base des modèles
+Base = declarative_base()
+
+# Fonction pour récupérer une session de base de données dans FastAPI
+async def get_db():
+    async with SessionLocal() as session:
+        yield session
